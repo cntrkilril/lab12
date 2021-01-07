a8cd42664c32316b314a07867d304d40dcd5abf8
Kirill Shalnov <shalkir2002@mail.ru>
2021-01-07 23:34:09 +0300

    Доделал приложение

--- script.js
+++ script.js
@@ -5,27 +5,41 @@ var App = {
             Familia: '',
             Name: '',
             notes: [],
-            visible: true
+            visible: true,
+            knopka: true
         }
     },
     methods: {
         Schetchik1(event) {
             this.Familia=event.target.value
             this.schet=this.Familia.length+this.Name.length
+            if (this.Familia!='' && this.Name!='') {
+                this.knopka=false
+            }
         },
         Schetchik2(event) {
             this.Name=event.target.value
             this.schet=this.Familia.length+this.Name.length
+            if (this.Familia!='' && this.Name!='') {
+                this.knopka=false
+            }
         },
         AddNewNote() {
-            this.notes.push(this.Familia+" "+this.Name)
-            this.Familia=" "
-            this.Name=" "
-            this.schet=0
-            this.visible=false
+            if (this.Familia!='' && this.Name!='') {
+                this.notes.push(ucFirst(this.Familia)+" "+ucFirst(this.Name))
+                this.Familia=""
+                this.Name=""
+                this.schet=0
+                this.visible=false
+                this.knopka=true
+            }
         }
     }
 }
 var app=Vue.createApp(App)
 var i=0;
 app.mount('#app')
+
+function ucFirst(str) {
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+  }
